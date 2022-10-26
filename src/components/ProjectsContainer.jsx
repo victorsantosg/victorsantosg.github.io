011387541899570a345d0d9c7574cf2f3578dc0f
Victor Santos <83377611+victorsantosg@users.noreply.github.com>
2022-10-25 22:13:05 -0300

    25-10

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,10 +3,7 @@ import React from "react";
 function ProjectsContainer () {
   return <section className="project-container">
     <h2>Projetos</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Consequuntur delectus harum quibusdam ex explicabo, veniam 
-        fugiat repudiandae pariatur saepe autem quam modi reprehenderit 
-        magni quod vero asperiores neque iste ad!
+    <p>Aqui você encontrará alguns dos meus projetos feitos no decorrer dessa jornada
     </p>
     <a href="#" className="btn">
         Ver Projeto
