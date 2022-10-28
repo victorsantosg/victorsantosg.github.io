7b0b7f6aeebb2e735d5419b510ac51639fd9a002
Victor Santos <83377611+victorsantosg@users.noreply.github.com>
2022-10-28 10:03:21 -0300

    git hub

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,14 +1,49 @@
 import React from "react";
+import '../styles/components/projectscontainer.sass';
+
+const projectsall = [
+  {id: 1, img: "public\contfinan2.png", link: "https://github.com/victorsantosg"},
+  //{id: 2, img: "public\contfinan2.png", link: "https://financascontrole.netlify.app"},
+]
 
 function ProjectsContainer () {
-  return <section className="project-container">
+  return ( 
+    <section>
+ {projectsall.filter(projects => projects.id).map(Filteredprojectsall => (
+    <>
     <h2>Projetos</h2>
-    <p>Aqui você encontrará alguns dos meus projetos feitos no decorrer dessa jornada
-    </p>
-    <a href="https://github.com/victorsantosg?tab=repositories" className="btn">
-        Ver Projeto
-    </a>
-  </section>
-}
+    <p>Aqui você encontrará alguns dos meus projetos feitos no decorrer dessa jornada</p>
+      <a href={Filteredprojectsall.link} className='btn' id={Filteredprojectsall.id} key={Filteredprojectsall.id}>
+      Projetos
+      </a>
+    </> 
+  ))}
 
+</section>
+
+)}
 export default ProjectsContainer
+
+
+
+
+
+    // <div className="projects-container">
+    //   <ul>
+    //     {projectsall.map(
+    //      (item) => 
+    //         item.id === '1' ? (
+    //         <>
+    //           <li key={id}>
+    //             <a className='btn' href={item.link}>Ver</a>
+    //           </li>  
+    //             <li key={id}>
+    //               {item.img}
+    //             </li> 
+    //        </>          
+    //      ) : (
+    //       []
+    //      )
+    //     )}
+    //   </ul>
+    // </div>
--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -5,7 +5,7 @@ import "../styles/components/socialnetworks.sass";
 
 const socialNetworks = [
     {id: 1, name: "linkedin", icon: <FaLinkedinIn/>, link: "https://www.linkedin.com/in/victor-santos-0a86021b7/"},
-    {id: 2, name: "github", icon: <FaGithub/>, link: "https://github.com/victorsantosg?tab=repositories"},
+    {id: 2, name: "github", icon: <FaGithub/>, link: "https://github.com/victorsantosg"},
     {id: 3, name: "instagram", icon: <FaInstagram/>, link: "https://careerkarma.com/blog/git-fatal-remote-origin-already-exists/"}
 ]
 
