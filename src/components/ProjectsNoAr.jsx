e00d00d79e45692c898fbec7b8032d725b81b251
Victor Santos <83377611+victorsantosg@users.noreply.github.com>
2022-11-07 18:07:09 -0300

    clima

--- src/components/ProjectsNoAr.jsx
+++ src/components/ProjectsNoAr.jsx
@@ -6,7 +6,7 @@ function ProjectsNoAr() {
     const projectsNoAr = [
         {id: 1, name: "Controle de Finanças", img: "https://gruponovus.com.br/wp-content/uploads/2020/06/Capa-finan%C3%A7as-768x432.jpg", link: "https://financascontrole.netlify.app/"},
         {id: 2, name: "Uniflix", img: "https://cdn.arstechnica.net/wp-content/uploads/2022/07/netflix-800x450.jpg", link: "https://uniflix.netlify.app/"},
-        // {id: 3, name: "", img: "https://gruponovus.com.br/wp-content/uploads/2020/06/Capa-finan%C3%A7as-768x432.jpg", link: "http://instagram.com/_u/victorsantosg_"},
+        {id: 3, name: "Clima", img: "https://www.fmmetropole.com.br/arquivos/cache/noticia/antigas/Previsao-do-Tempo/PREVISAO-DO-TEMPO-METROPOLE-FM-605x.jpg", link: "https://climavictor.netlify.app"},
     ]
 
     
