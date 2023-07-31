f1af68d1c6b1ad57d7a304eee7af62da0136d3bc
Victor Santos <83377611+victorsantosg@users.noreply.github.com>
2023-07-31 10:14:16 -0300

    Update ProjectsNoAr.jsx

--- src/components/ProjectsNoAr.jsx
+++ src/components/ProjectsNoAr.jsx
@@ -4,7 +4,7 @@ import "../styles/components/projectsnoar.sass";
 function ProjectsNoAr() {
 
     const projectsNoAr = [
-        {id: 1, name: "Controle de Finanças", img: "https://brasil.softlinegroup.com/uploads/cke/81/fd/25/Imagem36.jpg", link: "https://climavictor.netlify.app/"},
+        {id: 1, name: "Controle de Finanças", img: "https://brasil.softlinegroup.com/uploads/cke/81/fd/25/Imagem36.jpg", link: "https://financascontrole.netlify.app/"},
         {id: 2, name: "Uniflix", img: "https://cdn.arstechnica.net/wp-content/uploads/2022/07/netflix-800x450.jpg", link: "https://uniflix.netlify.app/"},
         {id: 3, name: "Clima", img: "https://www.fmmetropole.com.br/arquivos/cache/noticia/antigas/Previsao-do-Tempo/PREVISAO-DO-TEMPO-METROPOLE-FM-605x.jpg", link: "https://climavictor.netlify.app"},
     ]
