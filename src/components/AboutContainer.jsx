757d1de6e16a7e8e33f5cfc031a4c4617996bcd5
Victor Santos <83377611+victorsantosg@users.noreply.github.com>
2024-06-13 15:52:29 -0300

    atl

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function AboutContainer () {
     return <section className="about-container">
         <h2>Sobre</h2>
-        <p>Olá, sou o Victor! Tenho 27 anos e sou formado em Análise e Desenvolvimento de Sistemas na UNIFOR. Minha paixão pela tecnologia sempre esteve presente desde os meus 10 anos de idade. Hoje meu foco principal é voltado a Desenvolvimento web. Tenho conhecimentos em JavaScript, HTML, CSS, Node.js, GitHub, React.js, Next.js e estou mergulhando no TypeScript. Tenho também alguns cursos pela Udemy, dentre eles POO e React.js. Que, por sinal, também já vi e vejo na faculdade.
+        <p>Olá, sou o Victor! Tenho 27 anos e sou formado em Análise e Desenvolvimento de Sistemas na UNIFOR. Minha paixão pela tecnologia sempre esteve presente desde os meus 10 anos de idade. Hoje meu foco principal é voltado a Desenvolvimento web. Tenho conhecimentos em JavaScript, HTML, CSS, Node.js, GitHub, React.js, Vite.js e estou mergulhando no TypeScript. Tenho também alguns cursos pela Udemy, dentre eles POO e React.js. Que, por sinal, também já vi e vejo na faculdade.
         </p>
         <p>Tive uma experiência como desenvolvedor freelancer, com uma equipe fantástica que ajudou demais no meu crescimento profissional. Já participei de projetos em várias tecnologias como React.js, Angular e Laravel. Lá nós usávamos o Bitbucket para trabalhar o código entre a equipe e utilizávamos o Jira para organizar e dividir as tarefas. Já estou a algum tempo nessa aventura e sinto que estou cada vez mais capacitado para encarar novos desafios.
         </p>
