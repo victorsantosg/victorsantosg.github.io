5cf29613aef4b9319d385b40a6ff67eab68b4810
Victor Santos <83377611+victorsantosg@users.noreply.github.com>
2024-09-03 17:14:20 -0300

    atl

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -13,10 +13,10 @@ function ProjectsContainer () {
  {projectsall.filter(projects => projects.id).map(Filteredprojectsall => (
     <>
     <h2>Projetos</h2>
-    {/* <p>Aqui você encontrará alguns dos meus projetos feitos no decorrer dessa jornada</p> */}
-      {/* <a href={Filteredprojectsall.link} target='_blank' className='btn' id={Filteredprojectsall.id} key={Filteredprojectsall.id}>
+    <p>Aqui você encontrará alguns dos meus projetos feitos no decorrer dessa jornada</p>
+      <a href={Filteredprojectsall.link} target='_blank' className='btn' id={Filteredprojectsall.id} key={Filteredprojectsall.id}>
       Projetos
-      </a> */}
+      </a>
       <ProjectsNoAr className="project-externo"/>
     </> 
   ))}
