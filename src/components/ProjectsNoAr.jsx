df25fc164c7c5b7e7ba66df1fb4778ad4d0f106e
Victor Santos <83377611+victorsantosg@users.noreply.github.com>
2024-11-26 13:54:39 -0300

    atl

--- src/components/ProjectsNoAr.jsx
+++ src/components/ProjectsNoAr.jsx
@@ -4,7 +4,7 @@ import "../styles/components/projectsnoar.sass";
 function ProjectsNoAr() {
 
     const projectsNoAr = [
-        {id: 1, name: "Controle de Finanças", img: "https://brasil.softlinegroup.com/uploads/cke/81/fd/25/Imagem36.jpg", link: "https://financascontrole.netlify.app/"},
+        {id: 1, name: "Controle de Finanças", img: "https://img.freepik.com/vetores-premium/conhecimento-em-financas-e-investimentos_327176-1095.jpg?w=740", link: "https://financascontrole.netlify.app/"},
         {id: 2, name: "Uniflix", img: "https://cdn.arstechnica.net/wp-content/uploads/2022/07/netflix-800x450.jpg", link: "https://uniflix.netlify.app/"},
         {id: 3, name: "Clima", img: "https://www.fmmetropole.com.br/arquivos/cache/noticia/antigas/Previsao-do-Tempo/PREVISAO-DO-TEMPO-METROPOLE-FM-605x.jpg", link: "https://climavictor.netlify.app"},
         {id: 3, name: "Portfólio_estudo", img: "https://www.investopedia.com/thmb/bJ_bKFNuXw2BXn-23gldyoyLHZk=/2121x1414/filters:fill(auto,1)/GettyImages-508126658-e57932d9c64246e5b2946582a3586881.jpg", link:"https://main--portfoliovictorr.netlify.app/"}
@@ -22,7 +22,7 @@ function ProjectsNoAr() {
                   <br/>
                   <img src={filteredProwork.img} key={filteredProwork.id}/>
                   </a>
-                  <p>{filteredProwork.name}</p>
+                  <p id='p-project'>{filteredProwork.name}</p>
               </div>
               </>
             ))}
