d56d9266600cac92cea06ffbfddff4e2e902117f
Victor Santos <victoorsaantos16@gmail.com>
2024-11-26 13:16:26 -0300

    atl

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -5,17 +5,17 @@ function AboutContainer () {
     return <section className="about-container">
         <h2>Sobre</h2>
 
-        <p>Olá, eu sou o Victor!</p>
+        <p><strong>Olá, eu sou o Victor!</strong></p>
 
-       <p>Sou formado em Análise e Desenvolvimento de Sistemas pela UNIFOR e atualmente estou cursando uma Pós-Graduação em Desenvolvimento Web Full Stack na Faculdade INFNET, referência em tecnologia.</p>
+       <p>Sou formado em <strong>Análise e Desenvolvimento de Sistemas</strong> pela UNIFOR e atualmente estou cursando uma <strong>Pós-Graduação em Desenvolvimento Web Full Stack na Faculdade INFNET</strong>, referência em tecnologia.</p>
 
-        <p>Minha paixão por tecnologia começou cedo, aos 10 anos de idade, e hoje meu foco é o desenvolvimento web. Tenho experiência com diversas ferramentas e tecnologias, como JavaScript, HTML, CSS, Node.js, GitHub, React.js, Vite.js, Next.js, Express e estou me aprofundando em TypeScript. Além disso, já realizei cursos complementares pela Udemy, incluindo POO e React.js, que também foram abordados durante minha formação acadêmica.</p>
+        <p>Minha paixão por tecnologia começou cedo, aos 10 anos de idade, e hoje meu foco é o desenvolvimento web. Tenho experiência com diversas ferramentas e tecnologias, como <strong>JavaScript, HTML, CSS, Node.js, GitHub, React.js, Vite.js, Next.js</strong>, Express e estou me aprofundando em <strong>TypeScript.</strong> Além disso, já realizei cursos complementares pela <strong>Udemy</strong>, incluindo <strong>POO e React.js</strong>, que também foram abordados durante minha formação acadêmica.</p>
 
-        <p>Tive uma experiência enriquecedora como freelancer, colaborando com uma equipe incrível que impulsionou meu desenvolvimento profissional. Durante essa jornada, participei de projetos utilizando tecnologias como React.js, Angular e Laravel, e aprendi a lidar com ferramentas como Bitbucket para versionamento de código e Jira para organização e gerenciamento de tarefas.</p>
+        <p>Tive uma experiência enriquecedora como freelancer, colaborando com uma equipe incrível que impulsionou meu desenvolvimento profissional. Durante essa jornada, participei de projetos utilizando tecnologias como <strong>React.js, Angular e Laravel</strong>, e aprendi a lidar com ferramentas como <strong>Bitbucket</strong> para versionamento de código e <strong>Jira</strong> para organização e gerenciamento de tarefas.</p>
 
         <p>Ao longo dessa caminhada, me sinto cada vez mais preparado para enfrentar novos desafios e contribuir para projetos significativos. Meu objetivo atual é continuar crescendo neste mercado dinâmico e inovador.</p>
         <p>
-        Se você tem uma oportunidade ou quer conversar sobre tecnologia, estou à disposição! Vamos trocar ideias? 🚀
+        Se você tem uma oportunidade ou quer conversar sobre tecnologia, <strong>estou à disposição!</strong> Vamos trocar ideias? 🚀
         </p>
 
     </section>
